0b8517a02bd8cc4c94b39643a63fb3b27145c045
Vinicius Dias <vinicius-mdias@hotmail.com>
2024-05-07 18:49:44 -0300

    feat: Updates in prisma schema

--- src/pages/search/styles.ts
+++ src/pages/search/styles.ts
@@ -1,4 +1,4 @@
-import { Button, Heading, Text, styled } from "@pegasus-ui/react";
+import { Heading, styled } from "@pegasus-ui/react";
 
 export const Container = styled("div", {
   maxWidth: "calc(100vw - (100vw - 1160px) / 2)",
@@ -6,35 +6,6 @@ export const Container = styled("div", {
   display: "flex",
   alignItems: "center",
   gap: "$20",
-});
-
-export const Hero = styled("div", {
-  maxWidth: 480,
-  padding: "0 $10",
-
-  [`> ${Heading}`]: {
-    "@media (max-width: 600px)": {
-      fontSize: "$6xl",
-    },
-  },
-
-  [`> ${Text}`]: {
-    marginTop: "$2",
-    color: "$gray200",
-  },
-
-  [`> ${Button}`]: {
-    marginTop: "$2",
-  },
-});
-
-export const Preview = styled("div", {
-  paddingRight: "$8",
-  overflow: "hidden",
-
-  "@media (max-width: 600px)": {
-    display: "none",
-  },
 });
 
 export const ListContainer = styled("div", {
